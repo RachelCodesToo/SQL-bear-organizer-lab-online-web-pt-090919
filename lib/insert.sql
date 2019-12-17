8da72be4e11b5633b028c12bdadfa3223cf35913
Anne Fausto <a.r.fausto@gmail.com>
2019-12-17 18:52:28 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 8, "M", "brown", "sanguine", 1),
+("Rowdy", 3, "M", "white", "choleric", 1),
+("Tabitha", 5, "F", "gray", "melancholic", 0),
+("Seargent Brown", 2, "M", "white", "sanguine", 0),
+("Melissa", 1, "F", "black", "phlegmatic", 1),
+("Grinch", 2, "M", "gray", "melancholic", 1),
+("Wendy", 8, "F", "brown", "sanguine", 0),
+(null, 8, "M", "white", "phlegmatic", 1);
